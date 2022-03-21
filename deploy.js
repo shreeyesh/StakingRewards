b6c87df21391e84cee90db097830e4141589f229
trippdev.eth <shriyesh15@gmail.com>
2022-03-21 19:09:21 +0530

    Wrote the stakingRewards Contract and removed redundant files

---
+++ deploy.js
@@ -0,0 +1,15 @@
+import { any } from "hardhat/internal/core/params/argumentTypes";
+
+const { ethers, upgrades } = require("hardhat");
+StakingRewards : any
+
+async function main() {
+  const StakingRewards = await ethers.getContractFactory("StakingRewards");
+  const StakingRewards = await upgrades.deployProxy(StakingRewards, msg.sender, {
+    initializer: "initialize",
+});
+  await StakingRewards.deployed();
+  console.log("StakingRewards deployed to:", StakingRewards.address);
+}
+
+main();
