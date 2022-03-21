a0aff745c735da8ed9d3337e5bf05f2bb7d02661
trippdev.eth <shriyesh15@gmail.com>
2022-03-21 19:14:33 +0530

    Removed upgradeability from staking rewards for now

--- deploy.js
+++ deploy.js
@@ -1,13 +1,13 @@
 import { any } from "hardhat/internal/core/params/argumentTypes";
 
-const { ethers, upgrades } = require("hardhat");
+const { ethers} = require("hardhat");
 StakingRewards : any
 
 async function main() {
   const StakingRewards = await ethers.getContractFactory("StakingRewards");
-  const StakingRewards = await upgrades.deployProxy(StakingRewards, msg.sender, {
-    initializer: "initialize",
-});
+  // const StakingRewards = await upgrades.deployProxy(StakingRewards, msg.sender, {
+  //   initializer: "initialize",
+
   await StakingRewards.deployed();
   console.log("StakingRewards deployed to:", StakingRewards.address);
 }
